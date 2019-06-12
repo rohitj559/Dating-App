6ffdb0fc7c4f41ffccbd1e03b23c6b5722609e94
rohit <rohitj559@gmail.com>
2019-06-12 07:54:18 -0500

    trial

--- DatingApp/DatingApp.API/Controllers/ValuesController.cs
+++ DatingApp/DatingApp.API/Controllers/ValuesController.cs
@@ -14,7 +14,7 @@ namespace DatingApp.API.Controllers
         [HttpGet]
         public ActionResult<IEnumerable<string>> Get()
         {
-            return new string[] { "value1", "value2", "value3" };
+            return new string[] { "value1", "value2" };
         }
 
         // GET api/values/5
