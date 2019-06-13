73d302ed7275b3bc00fa8bf04dc7e016a446a5fa
rohit <rohitj559@gmail.com>
2019-06-12 19:55:05 -0500

    Creating, Retrieving data, modify endpoints ASYN

--- DatingApp/DatingApp.API/Controllers/ValuesController.cs
+++ DatingApp/DatingApp.API/Controllers/ValuesController.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using DatingApp.API.Data;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace DatingApp.API.Controllers
 {
@@ -10,18 +12,27 @@ namespace DatingApp.API.Controllers
     [ApiController]
     public class ValuesController : ControllerBase
     {
+        private readonly DataContext _context;
+
+        public ValuesController(DataContext context)
+        {
+            _context = context;
+        }
+
         // GET api/values
         [HttpGet]
-        public ActionResult<IEnumerable<string>> Get()
+        public async Task<IActionResult> GetValues()
         {
-            return new string[] { "value1", "value2" };
+            var values = await _context.Values.ToListAsync();
+            return Ok(values);
         }
 
         // GET api/values/5
         [HttpGet("{id}")]
-        public ActionResult<string> Get(int id)
+        public async Task<IActionResult> GetValue(int id)
         {
-            return "value";
+            var value = await _context.Values.Where( v => v.Id == id).FirstOrDefaultAsync();
+            return Ok(value);
         }
 
         // POST api/values
--- DatingApp/DatingApp.API/Data/DataContext.cs
+++ DatingApp/DatingApp.API/Data/DataContext.cs
@@ -5,7 +5,7 @@ namespace DatingApp.API.Data
 {
     public class DataContext: DbContext
     {
-        public DataContext(DbContextOptions<DbContext> options) : base(options){}
+        public DataContext(DbContextOptions<DataContext> options) : base(options){}
 
         public DbSet<Value> Values { get; set; }
     }
